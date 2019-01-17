1fe353b3b21b5b6d82ca14c00dac9b8fa8c6956b
zhoumou <zhoumou@ec2-3-0-0-103.ap-southeast-1.compute.amazonaws.com>
2019-01-17 09:35:23 +0800

    修改登录界面

--- src/app/routes/experiment/warning/warning.component.ts
+++ src/app/routes/experiment/warning/warning.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { webSocket } from 'rxjs/webSocket';
 
 @Component({
   selector: 'app-warning',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./warning.component.less']
 })
 export class WarningComponent implements OnInit {
-
+  ws: WebSocket; // 定义websocket
   constructor() { }
   data = [
     {
@@ -33,7 +34,55 @@ export class WarningComponent implements OnInit {
 
   ngOnInit() {
     this.start();
+    // this.webSocket();
+    this.connectWs();
   }
+  connectWs() {
+    if (this.ws != null) { this.ws.close(); }
+    this.ws = new WebSocket('ws://localhost:9998/echo');
+    const that = this;
+    this.ws.onopen = function (event) {
+      // socket 开启后执行，可以向后端传递信息
+      that.ws.send('sonmething');
+      alert('数据发送中...');
+    };
+    this.ws.onmessage = function (event) {
+      // socket 获取后端传递到前端的信息
+      that.ws.send('sonmething');
+      const received_msg = event.data;
+      // alert('数据已接收...');
+      console.log('event', received_msg);
+
+    };
+    this.ws.onerror = function (event) {
+      // socket error信息
+
+    };
+    this.ws.onclose = function (event) {
+      // socket 关闭后执行
+      alert('连接已关闭...');
+    };
+  }
+  // webSocket() {
+  //   // 打开一个 web socket
+  //   const ws = new WebSocket('ws://localhost:9998/echo');
+  //   ws.onopen = function () {
+  //     // Web Socket 已连接上，使用 send() 方法发送数据
+  //     ws.send('发送数据');
+  //     alert('数据发送中...');
+  //   };
+
+  //   ws.onmessage = function (evt) {
+  //     const received_msg = evt.data;
+  //     alert('数据已接收...');
+  //   };
+
+  //   ws.onclose = function () {
+  //     // 关闭 websocket
+  //     alert('连接已关闭...');
+  //   };
+
+  // }
   start() {
     const warning = document.getElementById('warning');
     const main = document.getElementById('main');
