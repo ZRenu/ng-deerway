3c16056a632d7da6ed3655bdaed7b7b94390a6aa
zhoumou <zhoumou@ec2-3-0-0-105.ap-southeast-1.compute.amazonaws.com>
2019-04-19 17:41:19 +0800

    新增htmal2canvas

--- src/app/routes/outside/html2canvas/html2canvas.component.ts
+++ src/app/routes/outside/html2canvas/html2canvas.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as html2canvas from 'html2canvas';
 @Component({
   selector: 'app-html2canvas',
   templateUrl: './html2canvas.component.html',
@@ -79,7 +79,42 @@ export class Html2canvasComponent implements OnInit {
   ngOnInit() {
   }
   saveImgLocal() {
+    const shareContent = document.getElementById('wrap'); // 需要截图的包裹的（原生的）DOM 对象
+    const width = shareContent.offsetWidth; // 获取dom 宽度
+    const height = shareContent.offsetHeight; // 获取dom 高度
+    const canvas = document.createElement('canvas'); // 创建一个canvas节点
+    const scale = 2; // 定义任意放大倍数 支持小数
+    canvas.width = width * scale; // 定义canvas 宽度 * 缩放
+    canvas.height = height * scale; // 定义canvas高度 *缩放
+    canvas.getContext('2d').scale(scale, scale); // 获取context,设置scale
+    const opts = {
+      scale: scale, // 添加的scale 参数
+      canvas: canvas, // 自定义 canvas
+      logging: true, // 日志开关
+      width: width, // dom 原始宽度
+      height: height, // dom 原始高度
+      useCORS: true // 【重要】开启跨域配置
+    };
+    // tslint:disable-next-line:no-shadowed-variable
+    html2canvas(shareContent, opts).then((canvas: any) => {
+      this.canvasImg = canvas.toDataURL('image/png');
+      this.downloadFile('导出图片', this.canvasImg);
+    });
 
   }
+  // filename: 文件名称， content: canvas图片流地址
+  downloadFile(filename, content) {
+    const base64Img = content;
+    const oA = document.createElement('a');
+    oA.href = base64Img;
+    oA.download = filename;
+    const event = document.createEvent('MouseEvents');
+    event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    oA.dispatchEvent(event);
+  }
+  download() {
+    this.downloadFile('导出图片', this.canvasImg);
+  }
+
 
 }
